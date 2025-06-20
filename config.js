0740e7d83f8d72cd25e6d8bf9c34094ff01041b0
savioshaju <savioshajum@gmail.com>
2025-06-20 20:23:12 +0530

    Delete Account

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-
 let apiUrl;
 
-if (location.hostname === 'localhost') {
+const isLocalhost = ['localhost', '127.0.0.1'].includes(location.hostname) || location.hostname.startsWith('192.168.');
+
+if (isLocalhost) {
   apiUrl = 'http://localhost:3000';
 } else {
   apiUrl = 'https://blogivea-backend.onrender.com';
