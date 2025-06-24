2968e5687a2b27ffa8b3a08afb7b2c2e2bad73b8
savioshaju <savioshajum@gmail.com>
2025-06-24 16:30:44 +0530

    testing

--- config.js
+++ config.js
@@ -4,11 +4,32 @@ const isLocalhost = ['localhost', '127.0.0.1'].includes(location.hostname) || lo
 
 if (isLocalhost) {
   apiUrl = 'http://localhost:3000';
- 
+  //apiUrl = 'https://blogivea-backend.onrender.com';
+
 }
 else {
 
   apiUrl = 'https://blogivea-backend.onrender.com';
-  
-  
+
+
 }
+
+(function checkAuthToken() {
+  const token = localStorage.getItem('authToken');
+  if (window.location.pathname.includes('loginpage.html')) return;
+  if (!token) return window.location.href = 'loginpage.html';
+
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const isExpired = payload.exp < Date.now() / 1000;
+    if (isExpired) {
+      localStorage.clear();
+      sessionStorage.clear();
+      alert("Session expired. Please login again.");
+      window.location.href = 'loginpage.html';
+    }
+  } catch {
+    localStorage.clear();
+    window.location.href = 'loginpage.html';
+  }
+})();
