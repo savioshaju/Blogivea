30bec2bdf42c65feca4b07f14f51a56c4a4054e9
savioshaju <savioshajum@gmail.com>
2025-07-07 20:29:37 +0530

    Search bar

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ let apiUrl;
 const isLocalhost = ['localhost', '127.0.0.1'].includes(location.hostname) || location.hostname.startsWith('192.168.');
 
 if (isLocalhost) {
-  apiUrl = 'http://localhost:3000';
-  //apiUrl = 'https://blogivea-backend.onrender.com';
+  //apiUrl = 'http://localhost:3000';
+  apiUrl = 'https://blogivea-backend.onrender.com';
 
 }
 else {
